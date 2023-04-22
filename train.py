fdff3efdb9f0437d6555e094f7a7805231749edc
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2023-04-22 03:04:41 +0200

    Added resume fit from checkpoint
    It checks if the checkpoint best_model.ckpt exist.
    If it does not exist, it starts from zero and run NUM_EPOCHS epochs.
    If it exist, loads the checkpoint and
    adds NUM_EPOCHS to the max_epochs of the trainer

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from model import LitCNN, CallbackLog_loss_per_epoch
 import lightning.pytorch as pl
 from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
+import os
 
 
 from lightning.pytorch.callbacks import Timer
@@ -13,16 +14,13 @@ pl.seed_everything(42, workers=True)
 
 if __name__ == "__main__":
 
-    model = LitCNN(config.N_CHANNELS, config.HIN, config.WIN,
-                   config.HOUT, config.WOUT, config.LEARNING_RATE)
-
     dm = RirDataModule(config.DATA_DIR, config.BATCH_SIZE, config.NUM_WORKERS)
 
     logger = TensorBoardLogger("tb_logs", name="my_model")
 
     early_stop_callback = EarlyStopping(
         monitor='val_loss',
-        patience=10,
+        patience=11,
         verbose=False,
         mode='min'
     )
@@ -31,42 +29,64 @@ if __name__ == "__main__":
     # model = LitCNN.load_from_checkpoint(checkpoint_path="./checkpoints/model-???.ckpt")
     checkpoint = ModelCheckpoint(
         dirpath='./checkpoints/',         # where to save the checkpoint
-        filename='model-{val_loss:.5f}',  # name of the model
+        filename='best_model',  # name of the model
         save_top_k=1,                     # save the best k models
-        monitor='val_loss',               # variable to monitor
+        monitor='train_loss',               # variable to monitor
         mode='min'                        # that has to be min or max
     )
 
-    # %% TRAINING Set Up
-    # Set the training options:
-    # -------------------------
+
+    model = LitCNN(
+        config.N_CHANNELS, config.HIN, config.WIN, 
+        config.HOUT, config.WOUT, config.LEARNING_RATE)
+
+    # %%
+    # If the restart file exist load it and run an extra NUM_EPOCHS
+    # Otherwise start from scratch and run NUM_EPOCHS
     
+    # temporal trainer object
+    trainer_temp = pl.Trainer()
+    
+    restart_file = None
+    max_epochs = config.NUM_EPOCHS
+
+    path_restart_file = os.path.join(checkpoint.dirpath, checkpoint.filename+'.ckpt')
+    if path_restart_file and os.path.isfile(path_restart_file):
+        restart_file = path_restart_file
+        # load checkpoint dictionary
+        dic_ckpt = trainer_temp.strategy.load_checkpoint(restart_file)
+
+        # Add extra epochs to train
+        max_epochs = dic_ckpt['epoch'] + config.NUM_EPOCHS
+        del trainer_temp, dic_ckpt
 
 
-    
+    # Set up the trainer with the number of epochs to train
     trainer = pl.Trainer(
         accelerator=config.ACCELERATOR,
         devices=config.DEVICES,
         min_epochs=1,
-        max_epochs=config.NUM_EPOCHS,
+        max_epochs=max_epochs,
         precision=config.PRECISION,
-        callbacks=[early_stop_callback, timer, checkpoint, CallbackLog_loss_per_epoch()],
+        # callbacks=[early_stop_callback, timer, checkpoint, CallbackLog_loss_per_epoch()],
+        callbacks=[timer, checkpoint, CallbackLog_loss_per_epoch()],
         logger=logger,
         deterministic=True,
         )
 
-    trainer.fit(model, dm)
-    trainer.validate(model, dm)
-    trainer.test(model, dm)
+    # %% FIT / VALIDATE / TEST
+    trainer.fit(model, dm, ckpt_path=restart_file)
 
-# %%
+    # trainer.validate(model, dm)
+    # trainer.test(model, dm)
 
-# query training/validation/test time (in seconds)
-_stage = "test" #train, validate, test
-a = timer.start_time(_stage)
-b = timer.end_time(_stage)
-c = timer.time_elapsed(_stage)
 
-print(f"end_time-start_time  = {b-a}")
-print(f"time_elapsed         = {c}")
+    # %%query training/validation/test time (in seconds)
+    _stage = "train" #train, validate, test
+    a = timer.start_time(_stage)
+    b = timer.end_time(_stage)
+    c = timer.time_elapsed(_stage)
+
+    print(f"Time elapsed in last run  = {b-a}")
+    print(f"Time elapsed in all runs  = {c}")
 
