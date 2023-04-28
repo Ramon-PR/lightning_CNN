ee87e735ba938b2722343faa6e93f677348b3495
Ramon Pozuelo <ramonpr@mech.kth.se>
2023-04-28 23:32:26 +0200

    path in erebos

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ NUM_EPOCHS = 13
 
 # Dataset
 DATA_DIR = r"C:\Users\keris\Desktop\Postdoc"
+DATA_DIR = r"/scratch/ramonpr/3NoiseModelling/"
 FILES_TRAIN = ["BalderRIR.mat", "FrejaRIR.mat"]
 FILES_VAL = ["MuninRIR.mat"]
 PARAM_DOWNSAMPLING = dict(ratio_t=1, ratio_x=0.5, kernel=[32, 32], stride=[32, 32])
@@ -16,11 +17,12 @@ N_FILTERS = [5, 5]
 CONV_KERN = [3, 5]
 CONV_PAD  = [1, 2]
 
-model_name = "LitCNN"
-SAVE_NAME = "CNN_basic"
+MODEL_NAME = "CNN_basic"
+SAVE_NAME = "CNN_5_5"
 
 
 # Compute related
 ACCELERATOR = "cpu"
 DEVICES = "auto"
 PRECISION = 32
+
