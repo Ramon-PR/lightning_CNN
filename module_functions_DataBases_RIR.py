b3ae3492c06c6ce474e821e5c7c9299bfc0d1465
Ramon Pozuelo Ruiz <pozueloruizramon@gmail.com>
2023-05-06 14:35:11 +0200

    Load Imag, x and time
    Load RIR, vector with x and time.
    In divide_RIR, also divide the x and time vectors for each image if passed as maskX or maskT

--- module_functions_DataBases_RIR.py
+++ module_functions_DataBases_RIR.py
@@ -16,13 +16,26 @@ def load_DB_ZEA(path):
     #Load data
     RIR=[]
     RIR = pymat.read_mat(path)["out"]["image"]
-    # T, M = RIR.shape
-    return RIR
+    fs = pymat.read_mat(path)["out"]["fs"] # Hz
+    
+    T, M = RIR.shape
+    
+    x0 = 0.0
+    dx = 0.03 # m
+    x = np.arange(0,M).reshape((1,M))*dx + x0
+
+    t0 = 0.0
+    dt = 1.0/fs # s
+    t = np.arange(0,T).reshape((T,1))*dt + t0
+    
+    # X, Time = np.meshgrid(x, t, indexing="xy")
+
+    return RIR, x, t #X, Time
 
 #-----------------------------------------------------------------------
 #---------- DIVIDE RIR IMAGE IN SUBSAMPLES -----------------------------
 #-----------------------------------------------------------------------
-def divide_RIR(RIR, maskX, kt=32, kx=32, strideT=32, strideX=32):
+def divide_RIR(RIR, maskX=None, maskT=None, kt=32, kx=32, strideT=32, strideX=32):
     # Divide RIR in small imagenes 
     # the image is a kernel of size (kt, kx) 
     # the kernel moves in strides strideX and strideT
@@ -31,6 +44,9 @@ def divide_RIR(RIR, maskX, kt=32, kx=32, strideT=32, strideX=32):
     
     T, M = RIR.shape
     
+    if kt > T or kx > M:
+        raise ValueError("The kernel size cannot be larger than the dimensions of RIR.")
+
     # stride = s
     # kernel = k
     # total: M
@@ -41,15 +57,31 @@ def divide_RIR(RIR, maskX, kt=32, kx=32, strideT=32, strideX=32):
 
     # Imags without modification
     Y = np.zeros([nImags,kt,kx])
-    subMask = np.ones([nImags,1,kx], dtype=bool)
 
     for imx in range(xImags):
         for imt in range(tImags):
             im = imx*tImags + imt
-            # Y[idx] is (nx,nt) if
             Y[im, :, :] = RIR[strideT*imt  : (strideT*imt)+kt, strideX*imx  : (strideX*imx)+kx]
-            subMask[im, 0,:] = maskX[0,strideX*imx  : (strideX*imx)+kx]            
-    return Y, subMask
+
+    if maskX is not None:
+        subMaskX = np.ones([nImags,1,kx], dtype=maskX.dtype)
+        for imx in range(xImags):
+            for imt in range(tImags):
+                im = imx*tImags + imt
+                subMaskX[im, 0,:] = maskX[0, strideX*imx  : (strideX*imx)+kx]            
+    else:
+        subMaskX = None
+    
+    if maskT is not None:
+        subMaskT = np.ones([nImags,kt,1], dtype=maskT.dtype)
+        for imx in range(xImags):
+            for imt in range(tImags):
+                im = imx*tImags + imt
+                subMaskT[im, :,0] = maskT[strideT*imt  : (strideT*imt)+kt, 0]
+    else:
+        subMaskT = None
+
+    return Y, subMaskX, subMaskT
 
 #-----------------------------------------------------------------------
 #---------- DOWNSAMPLE RIR IMAGE and GENERATE the General Masks --------
